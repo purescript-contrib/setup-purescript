5876b678c99173d931ead0241f20b05670b95ef8
Colin Wahl <cwahl@awakesecurity.com>
2020-09-09 20:29:38 -0700

    implement library with github-actions-toolkit library bindings

--- src/Main.purs
+++ src/Main.purs
@@ -2,18 +2,27 @@ module Main where
 
 import Prelude
 
+import Control.Monad.Except.Trans (ExceptT(..), runExceptT)
 import Data.Argonaut.Core (Json)
+import Data.Either (Either(..))
 import Data.Foldable (traverse_)
 import Effect (Effect)
-import Effect.Aff (launchAff_)
+import Effect.Aff (launchAff_, runAff_)
+import Effect.Class (liftEffect)
+import Effect.Exception (message)
+import GitHub.Actions.Core as Core
 import Setup.BuildPlan (constructBuildPlan)
 import Setup.GetTool (getTool)
 import Setup.UpdateVersions (updateVersions)
 
 main :: Json -> Effect Unit
-main json = do
-  tools <- constructBuildPlan json
-  launchAff_ $ traverse_ getTool tools
+main json = runAff_ go $ runExceptT do
+  tools <- ExceptT (liftEffect (runExceptT (constructBuildPlan json)))
+  traverse_ getTool tools
+  where
+  go res = case join res of
+    Left err -> Core.setFailed (message err)
+    Right a -> pure unit
 
 update :: Effect Unit
 update = launchAff_ updateVersions
