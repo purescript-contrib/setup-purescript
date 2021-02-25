b6b317c045fe8afd7ecb3978f7a6be1d6d37e14e
Thomas Honeyman <admin@thomashoneyman.com>
2021-02-25 14:32:25 -0800

    Fetch versions file remotely.

--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,10 @@ module Main where
 
 import Prelude
 
+import Affjax (printError)
+import Affjax as AX
+import Affjax.ResponseFormat as RF
 import Control.Monad.Except.Trans (ExceptT(..), mapExceptT, runExceptT)
-import Data.Argonaut.Parser as Json
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
 import Data.Foldable (traverse_)
@@ -12,22 +14,20 @@ import Effect.Aff (error, launchAff_, runAff_)
 import Effect.Class (liftEffect)
 import Effect.Exception (message)
 import GitHub.Actions.Core as Core
-import Node.Buffer as Buffer
-import Node.Encoding (Encoding(..))
-import Node.FS.Sync (readFile)
 import Setup.BuildPlan (constructBuildPlan)
 import Setup.GetTool (getTool)
 import Setup.UpdateVersions (updateVersions)
 
 main :: Effect Unit
 main = runAff_ go $ runExceptT do
-  versionsString <- liftEffect $ Buffer.toString UTF8 =<< readFile "./dist/versions.json"
-  versionsJson <- ExceptT $ pure $ lmap error $ Json.jsonParser versionsString
-  tools <- mapExceptT liftEffect $ constructBuildPlan versionsJson
+  versionsJson <- ExceptT $ map (lmap (error <<< printError)) $ AX.get RF.json versionsFile
+  tools <- mapExceptT liftEffect $ constructBuildPlan versionsJson.body
   liftEffect $ Core.info "Constructed build plan."
   traverse_ getTool tools
   liftEffect $ Core.info "Fetched tools."
   where
+  versionsFile = "https://raw.githubusercontent.com/purescript-contrib/setup-purescript/main/dist/versions.json"
+
   go res = case join res of
     Left err -> Core.setFailed (message err)
     Right a -> pure unit
