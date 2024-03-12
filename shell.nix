f282cb5a327ff7af0a7c961207c4283f3a9b6d07
Serhii Khoma <srghma@gmail.com>
2024-03-12 14:11:16 +0700

    feat: update spago

--- shell.nix
+++ shell.nix
@@ -1,14 +1,14 @@
 let
   pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/23.11.tar.gz";
   }) {};
 
   # 2021-08-05 nix-prefetch-git https://github.com/justinwoo/easy-purescript-nix
   pursPkgs = import (pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-purescript-nix";
-    rev = "bbef4245cd6810ea84e97a47c801947bfec9fadc";
-    sha256 = "00764zbwhbn61jwb5px2syzi2f9djyl8fmbd2p8wma985af54iwx";
+    rev = "117fd96acb69d7d1727df95b6fde9d8715e031fc";
+    sha256 = "1g91mzllzghm1x8y0np8vhrz3az087xipci23f28ax22w4h13hlm";
   }) { inherit pkgs; };
 
 in pkgs.stdenv.mkDerivation {
@@ -16,6 +16,6 @@ in pkgs.stdenv.mkDerivation {
   buildInputs = with pursPkgs; [
     pursPkgs.purs
     pursPkgs.spago
-    pkgs.nodejs-14_x
+    pkgs.nodejs-18_x
   ];
 }
