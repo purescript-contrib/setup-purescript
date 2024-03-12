0b17720ac671e55d54be7ad016ccc2df5b6caa15
Serhii Khoma <srghma@gmail.com>
2024-03-12 14:15:34 +0700

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    purescript-overlay = {
+      url = "github:thomashoneyman/purescript-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        config = { };
+        overlays = builtins.attrValues self.overlays;
+      });
+    in {
+      overlays = {
+        purescript = inputs.purescript-overlay.overlays.default;
+      };
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in {
+          default = pkgs.hello; # your package here
+        });
+
+      devShells = forAllSystems (system:
+        # pkgs now has access to the standard PureScript toolchain
+        let pkgs = nixpkgsFor.${system}; in {
+          default = pkgs.mkShell {
+            name = "my-purescript-project";
+            inputsFrom = builtins.attrValues self.packages.${system};
+            buildInputs = with pkgs; [
+              # watchexec
+              purs
+              spago-unstable
+              # purs-tidy-bin.purs-tidy-0_10_0
+              # purs-backend-es
+              # nodejs-16_x
+              # nodejs-18_x
+              nodejs_latest
+            ];
+
+            shellHook = ''
+              source <(spago --bash-completion-script `which spago`)
+              source <(node --completion-bash)
+            '';
+          };
+        });
+  };
+}
